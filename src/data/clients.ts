0ee76f32444613e153b3422c9868b4c2c51416fc
Tsundosika <ren706289@gmail.com>
2026-04-20 18:23:31 +0200

    add s42

--- src/data/clients.ts
+++ src/data/clients.ts
@@ -7,4 +7,5 @@ export type Client = {
 export const clients: Client[] = [
   { name: "einHolzkopf", href: "https://youtube.com/einholzkopf", avatar: "/clients/holzi.png" },
   { name: "Weichu", href: "https://www.youtube.com/channel/UCGzNqX7PxoGPmlClgBOU85w", avatar: "/clients/weichu.png" },
+  { name: "S42", href: "https://instagram.com/s42yt", avatar: "/clients/musa.png" },
 ];
